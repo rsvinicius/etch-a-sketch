a340610244fcb5c8b5b0dafa5cf861098dda1bdd
Vinicius R <vrodrigues.code@gmail.com>
2024-03-28 18:16:07 -0300

    chore: Add a button on the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed, and a new grid should be generated in the same total space as before

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-const numRows = 16;
-const numColumns = numRows; // Assuming a square grid
+let defaultGridSize = 16;
 const gridContainer = document.querySelector(".container");
+const resetButton = document.querySelector("button");
 
-for (let rowIndex = 0; rowIndex < numRows; rowIndex++) {
-    const gridRow = createGridRow(numColumns);
-    gridContainer.appendChild(gridRow);
+createSquareGrid(defaultGridSize);
+
+resetButton.addEventListener('click', () => {
+    resetGrid();
+});
+
+function resetGrid() {
+    const userInput = prompt('Enter the size of the grid (1-100):', '');
+    if (userInput !== null && userInput > 0 && userInput <= 100) {
+        clearGrid();
+        createSquareGrid(userInput);
+    } else {
+       alert('That\'s out of range! Try again.') 
+    }
 }
 
-function createGridRow(columnsPerRow) {
+function createSquareGrid(gridSize) {
+    document.documentElement.style.setProperty('--gridSize', gridSize);
+
+    for (let rowIndex = 0; rowIndex < gridSize; rowIndex++) {
+        const gridRow = createGridRow(gridSize);
+        gridContainer.appendChild(gridRow);
+    }
+}
+
+function createGridRow(elementsPerRow) {
     const rowElement = document.createElement("div");
     rowElement.classList.add("row");
 
-    for (let columnIndex = 0; columnIndex < columnsPerRow; columnIndex++) {
+    for (let columnIndex = 0; columnIndex < elementsPerRow; columnIndex++) {
         rowElement.appendChild(createSquare());
     }
 
@@ -23,10 +43,15 @@ function createSquare() {
     squareElement.classList.add("square");
 
     squareElement.addEventListener('mouseenter', (event) => {
-        // const square = event.target;
 
         squareElement.classList.add("hover-square");
     });
 
     return squareElement;
+}
+
+function clearGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild)
+    }
 }
